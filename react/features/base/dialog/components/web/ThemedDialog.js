3c2b72d99ca65b3de82f2a075680dbe25e47ba3d
Brian Moenga <brianmoenga@swiftlabs.ai>
2021-02-12 08:58:56 +0300

    Implement responsive behaviour on small screens.

--- react/features/base/dialog/components/web/ThemedDialog.js
+++ react/features/base/dialog/components/web/ThemedDialog.js
@@ -6,12 +6,12 @@ import {
     PositionerAbsolute,
     PositionerRelative
 } from '@atlaskit/modal-dialog/dist/es2019/styled/Modal.js';
-import { N0, DN50 } from '@atlaskit/theme/colors';
+import { NO, DN50 } from '@atlaskit/theme/colors';
 import { themed } from '@atlaskit/theme/components';
 import React from 'react';
 
 const ThemedDialog = props => {
-    const style = { backgroundColor: props.isChromeless ? 'transparent' : themed({ light: N0,
+    const style = { backgroundColor: props.isChromeless ? 'transparent' : themed({ light: NO,
         dark: DN50 })({ theme: { mode: 'dark' } }) };
 
     return (<Dialog
